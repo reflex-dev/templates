0bf1ea27dd1a342d5b7c37c62170ad65b70d5069
Carlos <cutillascarlos@gmail.com>
2024-06-07 20:47:37 +0200

    Update Customer Data App

---
+++ customer_data_app/customer_data_app/customer_data_app.py
@@ -0,0 +1,31 @@
+import reflex as rx
+from .components.stats_cards import stats_cards_group
+from .views.navbar import navbar
+from .views.table import main_table
+
+
+def index() -> rx.Component:
+    return rx.vstack(
+        navbar(),
+        stats_cards_group(),
+        rx.box(
+            main_table(),
+            width="100%",
+        ),
+        width="100%",
+        spacing="6",
+        padding_x=["1.5em", "1.5em", "3em"],
+    )
+
+
+app = rx.App(
+    theme=rx.theme(
+        appearance="dark", has_background=True, radius="large", accent_color="grass"
+    ),
+)
+
+app.add_page(
+    index,
+    title="Customer Data App",
+    description="A simple app to manage customer data.",
+)
---
+++ customer_data_app/rxconfig.py
@@ -0,0 +1,3 @@
+import reflex as rx
+
+config = rx.Config(app_name="customer_data_app")
