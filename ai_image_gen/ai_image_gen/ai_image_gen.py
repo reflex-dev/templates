41d64f9ff4c26fcc38ad31b2f8348b0c36ba4d9d
Carlos <cutillascarlos@gmail.com>
2024-06-24 16:47:41 +0200

    Move styles

--- ai_image_gen/ai_image_gen/ai_image_gen.py
+++ ai_image_gen/ai_image_gen/ai_image_gen.py
@@ -1,23 +1,12 @@
 import reflex as rx
+from . import styles
 
 # Import all the pages.
 from .pages import *
 
-base_stylesheets = [
-    "https://fonts.googleapis.com/css2?family=Roboto+Flex:wght@400;500;600;700;800&display=swap",
-    "react-zoom.css",
-]
-
-base_style = {
-    "font_family": "Roboto Flex",
-    rx.icon: {
-        "stroke_width": "1.75px",
-    },
-}
-
 app = rx.App(
-    style=base_style,
-    stylesheets=base_stylesheets,
+    style=styles.base_style,
+    stylesheets=styles.base_stylesheets,
     html_lang="en",
     html_custom_attrs={"className": "!scroll-smooth"},
     theme=rx.theme(
--- ai_image_gen/ai_image_gen/styles.py
+++ ai_image_gen/ai_image_gen/styles.py
@@ -29,3 +29,15 @@ button_props = {
 }
 
 box_shadow = "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)"
+
+base_stylesheets = [
+    "https://fonts.googleapis.com/css2?family=Roboto+Flex:wght@400;500;600;700;800&display=swap",
+    "react-zoom.css",
+]
+
+base_style = {
+    "font_family": "Roboto Flex",
+    rx.icon: {
+        "stroke_width": "1.75px",
+    },
+}
