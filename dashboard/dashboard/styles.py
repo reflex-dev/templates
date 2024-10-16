547a1601a55745863ea518a8acde6596af9f34f8
Olivier Bourdon <olivierbourdon38@gmail.com>
2024-10-16 10:38:28 +0200

    Fix for issue #38: Some CSS errors are displayed in browser developer tools inspector due to attributes mispelling

--- dashboard/dashboard/styles.py
+++ dashboard/dashboard/styles.py
@@ -68,9 +68,9 @@ ghost_input_style = {
     "--text-field-selection-color": "",
     "--text-field-focus-color": "transparent",
     "--text-field-border-width": "1px",
-    "background-clip": "content-box",
-    "background-color": "transparent",
-    "box-shadow": "inset 0 0 0 var(--text-field-border-width) transparent",
+    "backgroundClip": "content-box",
+    "backgroundColor": "transparent",
+    "boxShadow": "inset 0 0 0 var(--text-field-border-width) transparent",
     "color": "",
 }
 
