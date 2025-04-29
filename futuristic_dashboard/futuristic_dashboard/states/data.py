0245da0c35bbd1c9350ec19e8d4e5ccd9cabc647
pourhakimi <84860195+pourhakimi@users.noreply.github.com>
2025-04-29 16:33:50 +0300

    new dashboard for the templates project

---
+++ futuristic_dashboard/futuristic_dashboard/states/data.py
@@ -0,0 +1,155 @@
+stat_card_data = [
+    {
+        "title": "CPU Usage",
+        "value": "37%",
+        "sub_detail": "3.8 GHz | 12 Cores",
+        "icon": "cpu",
+        "color": "cyan",
+        "chart_data": [
+            {"v": 10},
+            {"v": 15},
+            {"v": 12},
+            {"v": 20},
+            {"v": 18},
+            {"v": 25},
+            {"v": 22},
+            {"v": 30},
+            {"v": 28},
+            {"v": 37},
+        ],
+    },
+    {
+        "title": "Memory",
+        "value": "74%",
+        "sub_detail": "16.4 GB / 24 GB",
+        "icon": "memory-stick",
+        "color": "purple",
+        "chart_data": [
+            {"v": 60},
+            {"v": 65},
+            {"v": 62},
+            {"v": 70},
+            {"v": 68},
+            {"v": 75},
+            {"v": 72},
+            {"v": 80},
+            {"v": 78},
+            {"v": 74},
+        ],
+    },
+    {
+        "title": "Network",
+        "value": "86%",
+        "sub_detail": "1.2 GB/s | 42ms",
+        "icon": "wifi",
+        "color": "teal",
+        "chart_data": [
+            {"v": 70},
+            {"v": 75},
+            {"v": 72},
+            {"v": 80},
+            {"v": 78},
+            {"v": 85},
+            {"v": 82},
+            {"v": 90},
+            {"v": 88},
+            {"v": 86},
+        ],
+    },
+]
+
+
+system_status_data = [
+    {
+        "name": "Core Systems",
+        "value": 89,
+        "color": "green",
+    },
+    {"name": "Security", "value": 75, "color": "cyan"},
+    {"name": "Network", "value": 86, "color": "blue"},
+]
+
+
+resource_allocation_data = [
+    {
+        "name": "Processing Power",
+        "value": 42,
+        "color": "cyan",
+    },
+    {
+        "name": "Memory Allocation",
+        "value": 68,
+        "color": "pink",
+    },
+    {
+        "name": "Network Bandwidth",
+        "value": 35,
+        "color": "blue",
+    },
+]
+
+
+quick_actions_data = [
+    {"name": "Security Scan", "icon": "shield-check"},
+    {"name": "Sync Data", "icon": "refresh-cw"},
+    {"name": "Backup", "icon": "database"},
+    {"name": "Console", "icon": "terminal"},
+]
+
+
+performance_chart_data = [
+    {
+        "time": "00:00",
+        "CPU": 28,
+        "Memory": 25,
+        "Network": 15,
+    },
+    {
+        "time": "03:00",
+        "CPU": 32,
+        "Memory": 30,
+        "Network": 20,
+    },
+    {
+        "time": "06:00",
+        "CPU": 30,
+        "Memory": 35,
+        "Network": 25,
+    },
+    {
+        "time": "09:00",
+        "CPU": 35,
+        "Memory": 40,
+        "Network": 30,
+    },
+    {
+        "time": "12:00",
+        "CPU": 38,
+        "Memory": 42,
+        "Network": 35,
+    },
+    {
+        "time": "15:00",
+        "CPU": 36,
+        "Memory": 40,
+        "Network": 32,
+    },
+    {
+        "time": "18:00",
+        "CPU": 34,
+        "Memory": 38,
+        "Network": 30,
+    },
+    {
+        "time": "21:00",
+        "CPU": 32,
+        "Memory": 35,
+        "Network": 28,
+    },
+    {
+        "time": "24:00",
+        "CPU": 30,
+        "Memory": 33,
+        "Network": 25,
+    },
+]
