b0804cc9ec537802a0ebee969da7c4c709ee2e14
Kastier1 <40179067+Kastier1@users.noreply.github.com>
2025-05-13 17:34:28 -0700

    idk +1

--- app/app/app/app.py
+++ app/app/app/app.py
@@ -1,5 +1,7 @@
 """Welcome to Reflex! This file outlines the steps to create a basic app."""
 
+"""Welcome to Reflex! This file outlines the steps to create a basic app."""
+
 import reflex as rx
 
 from rxconfig import config
