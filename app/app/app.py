9f5fecbf083aab2b4cd616f664049d22e2111e0c
Kastier1 <40179067+Kastier1@users.noreply.github.com>
2025-05-13 17:15:42 -0700

    bad boi

---
+++ app/app/app.py
@@ -0,0 +1,41 @@
+"""Welcome to Reflex! This file outlines the steps to create a basic app."""
+
+import reflex as rx
+
+from rxconfig import config
+from app.test import test
+
+
+class State(rx.State):
+    """The app state."""
+
+    ...
+
+
+def index() -> rx.Component:
+    # Welcome Page (Index)
+    return rx.container(
+        rx.color_mode.button(position="top-right"),
+        rx.vstack(
+            rx.heading("Welcome to Reflex!", size="9"),
+            rx.text(
+                "Get started by editing ",
+                rx.code(f"{config.app_name}/{config.app_name}.py"),
+                size="5",
+            ),
+            rx.link(
+                rx.button("Check out our docs!"),
+                href="https://reflex.dev/docs/getting-started/introduction/",
+                is_external=True,
+            ),
+            spacing="5",
+            justify="center",
+            min_height="85vh",
+        ),
+        rx.logo(),
+    )
+
+
+app = rx.App()
+app.add_page(index)
+app._api.add_route("/test", test, methods=["GET"])
---
+++ app/app/test.py
@@ -0,0 +1,6 @@
+async def test():
+    """
+    Test function to check if the application is running.
+    """
+    print("hi")
+    return {"message": "Test successful!"}
